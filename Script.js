94d60398c411dfacbdca9c20b81434e95cc95951
TheRagingAsura <undertakerryu@gmail.com>
2022-01-25 12:28:15 -0500

    updates

--- Script.js
+++ Script.js
@@ -1,34 +1,3 @@
-const twitter = "@ElonMusk";
+const course = "JavaScript 101: coding for beginners";
 
-if (twitter == "@theragingasura") {
-    //execute the code below
-    console.log("My Twitter handle is", twitter);
-} else {
-    //execute the code below if not theragingasura
-    console.log(twitter, "is the twitter handle");
-}
-
-const age = 17;
-if(age >= 18){
-    console.log("They are allowed to vote.");
-}else {
-    console.log("They are not able to vote.")
-}
-if (age >=21) {
-    console.log("And they are allowed to drink.");
-} else {
-    console.log("Not old enough to drink.")
-}
-
-
-//This is an example of an else if statement, basically a neater way of nesting.
-const user ="John"; //In here you can add any name then get the results from below.
-
-if(user == "Nikolai"){
-    console.log("Hi Nikolai");
-} else if (user == "John") {
-    console.log("Hi John");
-}
-    else {
-        console.log("Who are you?")
-    }
+let num = "012345";
