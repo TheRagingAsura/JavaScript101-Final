ffa2471f59ca207316b6ab016b91fdd8fb171bd0
TheRagingAsura <undertakerryu@gmail.com>
2022-02-07 16:22:27 -0500

    Updates for Final 101

--- Script.js
+++ Script.js
@@ -1,10 +1,24 @@
-function addNumbers(greetingName, ...numbers) {
-  let total = 0;
-  for(index in numbers){
-    total = total + numbers[index];
-  }
-return `${greetingName} the total is ${total}`;
+// Step 1: Fill in these three consts by selecting the right nodes by their HTML ID's
+const name = document.getElementById(""); // Fill this in 
+const age = document.getElementById(""); // Fill this in.
+const dogYears = document.getElementById(""); // Fill this in. 
+
+// Step 2. Set the values.
+// Example: myNode.innerText = "something"
+// Set `name` and `age` in here
+
+
+function showDogYears(age) {
+  // Step 3. Cast `age` as a number
+  dogYears.innerText = age * 7;
+  return age * 7
 }
 
-const newTotal = addNumbers("John", 1, 2);
-console.log(newTotal);
+// Step 4. Set the parameter of showDogyears()
+dogYears.innerText = showDogYears()
+
+// Step 5. Select all the .fake-input classes and give them the class of .real-input. This may take a little "googling" to find the solution, but that's half of what web development is all about. 
+const fakeInputs = document.querySelectorAll(".fake-input")
+fakeInputs.forEach(node => {
+  node.classList.add()
+})
