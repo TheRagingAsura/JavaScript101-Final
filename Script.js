717de3656ab900141690d586b37b58c39b2eef94
TheRagingAsura <undertakerryu@gmail.com>
2022-02-07 18:24:59 -0500

    final update one

--- Script.js
+++ Script.js
@@ -1,15 +1,18 @@
 // Step 1: Fill in these three consts by selecting the right nodes by their HTML ID's
-const name = document.getElementById(""); // Fill this in 
-const age = document.getElementById(""); // Fill this in.
-const dogYears = document.getElementById(""); // Fill this in. 
+const name = document.getElementById("name"); // Fill this in 
+const age = document.getElementById("age"); // Fill this in.
+const dogYears = document.getElementById("dogyears"); // Fill this in. 
 
 // Step 2. Set the values.
 // Example: myNode.innerText = "something"
 // Set `name` and `age` in here
 
+name.innerText = "Nikolai";
+age.innerText = 23;
 
 function showDogYears(age) {
   // Step 3. Cast `age` as a number
+  age = Number(age);
   dogYears.innerText = age * 7;
   return age * 7
 }
